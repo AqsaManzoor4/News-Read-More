09264a324f11384cb05268bcb2596c7c22d1d928
AqsaManzoor4 <aqsa34723@gmail.com>
2024-12-14 04:14:59 -0800

    Done Bro

---
+++ app.js
@@ -0,0 +1,52 @@
+const readmore = document.getElementById("readmore");
+const texthidden = document.getElementById("texthidden")
+
+readmore.onclick = () => {
+    if (texthidden.style.display === "none"){
+        readmore.textContent = "Read Less";
+        texthidden.style.display = "inline";
+    }
+    else {
+        readmore.textContent = "Read More";
+        texthidden.style.display = "none";
+    }
+}
+const readmore1 = document.getElementById("readmore1");
+const texthidden1 = document.getElementById("texthidden1")
+
+readmore1.onclick = () => {
+    if (texthidden1.style.display === "none"){
+        readmore1.textContent = "Read Less";
+        texthidden1.style.display = "inline";
+    }
+    else {
+        readmore1.textContent = "Read More";
+        texthidden1.style.display = "none";
+    }
+}
+const readmore2 = document.getElementById("readmore2");
+const texthidden2 = document.getElementById("texthidden2")
+
+readmore2.onclick = () => {
+    if (texthidden2.style.display === "none"){
+        readmore2.textContent = "Read Less";
+        texthidden2.style.display = "inline";
+    }
+    else {
+        readmore2.textContent = "Read More";
+        texthidden2.style.display = "none";
+    }
+}
+const readmore3 = document.getElementById("readmore3");
+const texthidden3 = document.getElementById("texthidden3")
+
+readmore3.onclick = () => {
+    if (texthidden3.style.display === "none"){
+        readmore3.textContent = "Read Less";
+        texthidden3.style.display = "inline";
+    }
+    else {
+        readmore3.textContent = "Read More";
+        texthidden3.style.display = "none";
+    }
+}
